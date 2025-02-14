30e8e99bcd3be58aac640a45e0eb50fc1e4d26b5
Baljender Singh Bamrah <156078007+Baljender-Singh-Bamrah@users.noreply.github.com>
2025-02-14 18:19:07 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React, { useState } from 'react'
 
 const App = () => {
   let [bg,changebg] = useState("black")
-  let st= "border-2 border-black p-1 rounded-lg text-2xl p-2 w-25 text-center";
+  let st= "border-2 border-black p-1 rounded-lg text-2xl p-2 w-25 text-center cursor-pointer";
   return (
     <>
     <div className='h-screen grid items-end justify-center body' style={{backgroundColor:bg}}>
           <ul className='text-white flex gap-5 m-8 p-5 border-4  rounded-2xl bg-indigo-300 border-black flex-wrap'>
-            <li className={`${st}  bg-red-500`} onClick={()=>{changebg("red")}}>Red</li>
+            <li className={`${st} bg-red-500`} onClick={()=>{changebg("red")}}>Red</li>
             <li className={`${st} bg-blue-600`} onClick={()=>{changebg("blue")}}>Blue</li>
             <li className={`${st} bg-green-500`} onClick={()=>{changebg('green')}}>Green</li>
             <li className={`${st} bg-white text-black`} onClick={()=>{changebg("white")}}>White</li>
